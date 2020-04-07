8f6a9a63f4741aaff9c4edeeee12be1f5fe74e75
yoon54 <46500698+yoon54@users.noreply.github.com>
2020-04-07 14:47:39 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> newObject = new List<object>();
+            newObject.Add(7);
+            newObject.Add(28);
+            newObject.Add(-1);
+            newObject.Add(true);
+            newObject.Add("chair");
+            int sum = 0;
+            for (var idx = 0; idx < newObject.Count; idx++)
+                {
+                    Console.WriteLine(newObject[idx]);
+                }
+            foreach(object thing in newObject)
+            {
+                if(thing is int)
+                {
+                    sum = sum + (int)thing;
+                }
+            
+        }
+        Console.WriteLine(sum);
+    }
+    }
+}
